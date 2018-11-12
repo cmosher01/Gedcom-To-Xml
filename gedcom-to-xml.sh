f22ed2c8a9631dd4c5a4eebff8e50f8deb25d08f
Christopher Mosher <cmosher@coreinformatics.com>
2018-11-12 17:50:34 -0500

    shell script: use calabash cmd, fix readlink for mac

--- gedcom-to-xml.sh
+++ gedcom-to-xml.sh
@@ -1,19 +1,26 @@
-#!/bin/sh
-calabash=/opt/calabash/current/xmlcalabash-*.jar
+#!/bin/sh -e
 
 if [ $# = 0 ] ; then
     echo "usage: $0 input.ged" >&2
     echo "Converts input.ged to XML on standard output." >&2
+    echo "Use - to read from standard input." >&2
     exit 1
 fi
 
-cj="$(readlink -f $calabash)"
-echo "Using Calabash jar file: $cj" >&2
-if [ ! -r "$cj" ] ; then
-    echo "Error: cannot find Calabash jar file $calabash" >&2
+echo "Checking for installation of Calabash XML." >&2
+if ! calabash -i source= -s p:count >/dev/null 2>&1 ; then
+    echo "Please install calabash to run the pipeline." >&2
+    calabash
     exit 1
 fi
 
-here="$(dirname "$(readlink -f "$0")")"
+in="$1"
+if [ "$in" = "-" ] ; then
+    in=$(mktemp)
+    cat - >$in
+fi
 
-java -jar "$cj" -p base-dir=file://$(pwd)/ -p filename="$1" $here/gedcom.xpl
+me="$(perl -MCwd -e 'print Cwd::abs_path shift' "$0")"
+here="$(dirname "$me")"
+
+calabash -p base-dir=file://$(pwd)/ -p filename="$in" $here/gedcom.xpl
